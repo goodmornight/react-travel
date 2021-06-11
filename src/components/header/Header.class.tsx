b30c416179ce7225aa7004b18355dab3a6d22b05
GoodNight <goodnightlilanhui@gmail.com>
2021-06-11 20:35:06 +0800

    refactor: redux

--- src/components/header/Header.class.tsx
+++ src/components/header/Header.class.tsx
@@ -5,10 +5,14 @@ import { Layout, Typography, Input, Menu, Button, Dropdown } from "antd";
 import { GlobalOutlined } from "@ant-design/icons";
 import { withRouter, RouteComponentProps } from "react-router-dom";
 import store from "../../redux/store";
-import { LanguageState } from "../../redux/languageReducer";
+import { LanguageState } from "../../redux/language/languageReducer";
+import {
+  addLanguageActionCreator,
+  changeLanguageActionCreator
+} from "../../redux/language/languageActions";
 import { withTranslation, WithTranslation } from "react-i18next";
 
-interface State extends LanguageState {}
+interface State extends LanguageState { }
 
 class HeaderComponnet extends React.Component<
   RouteComponentProps & WithTranslation,
@@ -36,16 +40,10 @@ class HeaderComponnet extends React.Component<
     console.log(e);
     if (e.key === "new") {
       // 处理新语言添加action
-      const action = {
-        type: "add_language",
-        payload: { code: "new_lang", name: "新语言" },
-      };
+      const action = addLanguageActionCreator("new_lang", "新语言");
       store.dispatch(action);
     } else {
-      const action = {
-        type: "change_language",
-        payload: e.key,
-      };
+      const action = changeLanguageActionCreator(e.key);
       store.dispatch(action);
     }
   };
