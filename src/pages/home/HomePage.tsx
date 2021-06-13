310a0995df139e070b227ddf492d98fa2631c7d4
GoodNight <goodnightlilanhui@gmail.com>
2021-06-13 11:12:50 +0800

    feat: add api

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -7,22 +7,72 @@ import {
   ProductCollection,
   BusinessPartners,
 } from "../../components";
-import { Row, Col, Typography } from "antd";
-import { productList1, productList2, productList3 } from "./mockups";
+import { Row, Col, Typography, Spin } from "antd";
 import sideImage from "../../assets/images/sider_2019_12-09.png";
 import sideImage2 from "../../assets/images/sider_2019_02-04.png";
 import sideImage3 from "../../assets/images/sider_2019_02-04-2.png";
 import styles from "./HomePage.module.css";
 import { withTranslation, WithTranslation } from "react-i18next";
- 
-class HomePageComponent extends React.Component<WithTranslation> {
+import axios from "axios";
+
+interface State {
+  loading: boolean,
+  error: string | null,
+  productList: any[]
+}
+
+class HomePageComponent extends React.Component<WithTranslation, State> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      error: null,
+      productList: []
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const { data } = await axios.get('http://123.56.149.216:8080/api/productCollections');
+      this.setState({
+        loading: false,
+        error: null,
+        productList: data
+      })
+    } catch (err) {
+      this.setState({
+        loading: false,
+        error: err.message,
+      })
+    }
+
+  }
+
   render() {
-    // console.log(this.props.t)
     const { t } = this.props;
+    const { productList, loading, error } = this.state;
+
+    if(loading) {
+      return (<Spin
+        size="large"
+        style={{
+          marginTop: 200,
+          marginBottom: 200,
+          marginLeft: "auto",
+          marginRight: "auto",
+          width: "100%",
+        }}
+      />)
+    }
+
+    if(error) {
+      return <div>网站出错：{error}</div>;
+    }
+
     return (
       <>
         <Header />
-        {/* 页面内容 content */}
         <div className={styles["page-content"]}>
           <Row style={{ marginTop: 20 }}>
             <Col span={6}>
@@ -39,7 +89,7 @@ class HomePageComponent extends React.Component<WithTranslation> {
               </Typography.Title>
             }
             sideImage={sideImage}
-            products={productList1}
+            products={productList[0].touristRoutes}
           />
           <ProductCollection
             title={
@@ -48,7 +98,7 @@ class HomePageComponent extends React.Component<WithTranslation> {
               </Typography.Title>
             }
             sideImage={sideImage2}
-            products={productList2}
+            products={productList[1].touristRoutes}
           />
           <ProductCollection
             title={
@@ -57,7 +107,7 @@ class HomePageComponent extends React.Component<WithTranslation> {
               </Typography.Title>
             }
             sideImage={sideImage3}
-            products={productList3}
+            products={productList[2].touristRoutes}
           />
           <BusinessPartners />
         </div>
