49a770a23a09f6617abb2103565a214abd98adac
GoodNight <goodnightlilanhui@gmail.com>
2021-06-14 16:59:32 +0800

    feat: add signin redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -6,12 +6,14 @@ import { actionLog } from "./middlewares/actionLog";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { productDetailSlice } from "./productDetail/slice";
 import { productSearchSlice } from "./productSearch/slice";
+import { userSlice } from "./user/slice";
 
 const rootReducers = combineReducers({
   language: languageReducer,
   recommendProducts: recommendProductsReducer,
   productDetail: productDetailSlice.reducer,
-  productSearch: productSearchSlice.reducer
+  productSearch: productSearchSlice.reducer,
+  user: userSlice.reducer
 })
 
 // const store = createStore(rootReducers,applyMiddleware(thunk, actionLog));
---
+++ src/redux/user/slice.ts
@@ -0,0 +1,52 @@
+import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+interface UserState {
+  token: string | null,
+  loading: boolean,
+  error: string | null
+}
+
+const initialState: UserState = {
+  token: null,
+  loading: false,
+  error: null
+}
+
+export const signIn = createAsyncThunk(
+  "user/signIn",
+  async (paramaters: {
+    email: string,
+    password: string
+  }, thunkAPI) => {
+    const { data } = await axios.post(
+      `http://123.56.149.216:8080/auth/login`, {
+        email: paramaters.email,
+        password: paramaters.password
+      }
+    );
+    return data.token;
+  }
+);
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [signIn.pending.type]: (state) => {
+      // return { ...state, loading: true };
+      state.loading = true;
+    },
+    [signIn.fulfilled.type]: (state, action) => {
+      state.token = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    [signIn.rejected.type]: (state, action: PayloadAction<string | null>) => {
+      //   const ddd = action.payload;
+      state.loading = false;
+      state.error = action.payload;
+    },
+  }
+})
