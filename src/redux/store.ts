0d868dd7540a6beafe99f78b1eda299b2a47dda7
GoodNight <goodnightlilanhui@gmail.com>
2021-06-14 20:26:07 +0800

    feat: order reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -10,6 +10,7 @@ import { userSlice } from "./user/slice";
 import { shoppingCartSlice } from './shoppingCart/slice';
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import { orderSlice } from "./order/slice";
 
 const persistConfig = {
   key: "root",
@@ -23,7 +24,8 @@ const rootReducers = combineReducers({
   productDetail: productDetailSlice.reducer,
   productSearch: productSearchSlice.reducer,
   user: userSlice.reducer,
-  shoppingCart: shoppingCartSlice.reducer
+  shoppingCart: shoppingCartSlice.reducer,
+  order: orderSlice.reducer
 })
 
 
