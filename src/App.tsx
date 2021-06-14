2313de880b8cd0eb3d429d7631fc9c68e6c68a86
GoodNight <goodnightlilanhui@gmail.com>
2021-06-14 17:52:54 +0800

    feat: add shoppingCart page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import styles from "./App.module.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { HomePage, SignInPage, RegisterPage, DetailPage, SearchPage } from "./pages";
+import { HomePage, SignInPage, RegisterPage, DetailPage, SearchPage, ShoppingCartPage } from "./pages";
+import { Redirect } from 'react-router';
+import { useSelector } from "./redux/hooks";
+
+const PrivateRoute = ({ component, isAuthenticated, ...rest }) => {
+  const routeComponent = (props) => {
+    return isAuthenticated ? (
+      React.createElement(component, props)
+    ) : (
+      <Redirect to={{pathname: "/signIn"}} />
+    )
+  }
+  return <Route render={routeComponent} {...rest} />
+}
 
 function App() {
+  const jwt = useSelector((s) => s.user.token);
   return (
     <div className={styles.App}>
       <BrowserRouter>
@@ -13,6 +27,11 @@ function App() {
           <Route path="/register" component={RegisterPage} />
           <Route path="/detail/:touristRouteId" component={DetailPage} />
           <Route path="/search/:keywords?" component={SearchPage} />
+          <PrivateRoute
+            isAuthenticated={jwt !== null}
+            path="/shoppingCart"
+            component={ShoppingCartPage}
+          />
           <Route render={() => <h1>404 not found</h1>} />
         </Switch>
       </BrowserRouter>
